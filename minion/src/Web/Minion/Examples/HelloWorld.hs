22d393c58a35d68f7e330d6116d9da3e352eca94
Danil Berestov <goosedb@yandex.ru>
2024-10-08 19:08:16 +0300

    fix openapi3 extra files

--- minion/src/Web/Minion/Examples/HelloWorld.hs
+++ minion/src/Web/Minion/Examples/HelloWorld.hs
@@ -6,7 +6,7 @@ module Web.Minion.Examples.HelloWorld (app) where
 import Web.Minion
 
 app :: ApplicationM IO
-app = serve api 
+app = serve api
 
 api :: Router Void IO
 api =
@@ -14,6 +14,6 @@ api =
     /> [ "about" /> handlePlainText @String GET about
        , "hello" /> capture @String "name" .> handlePlainText @String GET hello
        ]
-  where 
-    about = pure "Hello-World Minion server"
-    hello name = pure $ "Hello, " <> name <> "!"
+ where
+  about = pure "Hello-World Minion server"
+  hello name = pure $ "Hello, " <> name <> "!"
